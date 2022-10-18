f95981a9cea06809f6b12e270bb2288e1dbccbef
Gleb Zverev <glebzverev2001@gmail.com>
2022-10-18 21:08:40 +0300

    added approve button

--- src/Valve/Valve.js
+++ src/Valve/Valve.js
@@ -1,11 +1,14 @@
 import { Button, Input, Stack } from '@chakra-ui/react';
 import {collectionExist, getCollectionOwners, SplitRevenue} from '../Utils/InfoGette'
 import { ethers} from "ethers";
+import {useState} from "react";
+import IERC20ABI from "../abi/IERC20ABI.json";
 
 async function Split(){
     const amount = document.getElementById('amount').value;
     const collectionName = document.getElementById('name').value;
     const token = document.getElementById('token').value;
+
     let collectionAddress = await collectionExist(collectionName);
     let temp = document.getElementById('indexes').value;
     let indexes = temp.split(', ');
@@ -25,11 +28,32 @@ async function Split(){
 }
 
 function Valve() {
+    const [amountIn, setAmountIn] = useState('');
+    const [address, setAddress] = useState('0x222482c6ac8d42d2cdcc75e94cdc2fd9820ef512');
+    const [tokenAddress, setTokenAddress] = useState('');
+ 
+
+        
+    async function approve(){
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(
+            tokenAddress,
+            IERC20ABI,
+            signer
+            );
+            try {
+                console.log("try Swap");
+                const amount = ethers.utils.parseEther((amountIn).toString()); 
+                let response = await contract.approve(address, amount);
+            } catch (err) {
+            console.log("error: ",err);
+            }
+        }
+    }
   return (
     <div>
-        {/* <div>
-            In Progress . . .
-        </div> */}
         <div>
             <Stack spacing={4} align='stretch'>
                 <Input width='auto' placeholder='Collection name' id="name" text="Collection name" type="string" />
@@ -38,6 +62,31 @@ function Valve() {
                 <Input width='auto' placeholder='Index of NFTs' id="indexes" text="Indexes of NFTs" type="string" />
                 <Button onClick={Split}>
                     Split revenue
+                </Button>
+            </Stack>
+            <Stack spacing={4} align='stretch'>
+                <Input width='auto' 
+                value={tokenAddress}
+                onChange={(e) => setTokenAddress(e.target.value)} 
+                placeholder='Token address to Approve' 
+                text="Token address to Approve" 
+                type="address" />
+                
+                <Input width='auto' 
+                value={amountIn}
+                onChange={(e) => setAmountIn(e.target.value)} 
+                placeholder='Approve amount' 
+                text="Approve amount" 
+                type="number" />
+                
+                <Input width='auto' 
+                value={address}
+                onChange={(e) => setAddress(e.target.value)} 
+                placeholder='Approve address'
+                text="Approve address" 
+                type="address" />
+                <Button onClick={approve}>
+                    Approve
                 </Button>
             </Stack>
         </div>
